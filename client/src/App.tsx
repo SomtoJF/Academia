47abbc8d9b4bc7c509db1a52655ca2da28df6bff
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-02-08 22:50:11 +0100

    Create router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,23 @@
 import "./App.sass";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/components/Home";
+import Login from "./pages/components/Login";
+import Signup from "./pages/components/Signup";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Home />,
+	},
+	{
+		path: "/login",
+		element: <Login />,
+	},
+	{ path: "/signup", element: <Signup /> },
+]);
 
 function App() {
-	return <div>app</div>;
+	return <RouterProvider router={router} />;
 }
 
 export default App;
