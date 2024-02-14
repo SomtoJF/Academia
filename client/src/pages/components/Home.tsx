7afb8241c7f78df5cd1e5b2ac6cb6d4e4b2e1e71
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-02-14 23:44:12 +0100

    Move page styles to .page selector

--- client/src/pages/components/Home.tsx
+++ client/src/pages/components/Home.tsx
@@ -1,12 +1,10 @@
 import Hero from "../../features/home/components/Hero";
-import Navbar from "../../features/home/components/Navbar";
 import Support from "../../features/home/components/Support";
 import "../styles/Home.styles.sass";
 
 export default function Home() {
 	return (
 		<div id="home">
-			<Navbar />
 			<Hero />
 			<Support />
 		</div>
