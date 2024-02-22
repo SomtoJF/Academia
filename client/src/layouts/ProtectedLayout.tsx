bd2c4a3e03e083f146c70c15a29b0a5e2e56cbf1
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-02-22 14:33:34 +0100

    Render sidebar in protected layout

--- client/src/layouts/ProtectedLayout.tsx
+++ client/src/layouts/ProtectedLayout.tsx
@@ -1,7 +1,41 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
+import { gql, useQuery } from "@apollo/client";
+import Sidebar from "../components/sidebar/Sidebar";
+import { Spin } from "antd";
+
+const FETCH_USER_ROLE = gql`
+	query fetchUserRole($id: ID!) {
+		user(id: $id) {
+			role
+		}
+	}
+`;
 
 export default function ProtectedLayout() {
 	const { currentUser } = useAuth();
-	return currentUser ? <Outlet /> : <Navigate to={"/login"} />;
+	console.log(currentUser);
+	const { loading } = useQuery(FETCH_USER_ROLE, {
+		variables: { id: currentUser?.uid },
+		onCompleted(data) {
+			console.log(data);
+		},
+	});
+
+	return (
+		<div className="protected-page">
+			<Sidebar />
+			{currentUser ? (
+				loading ? (
+					<Spin />
+				) : (
+					<main>
+						<Outlet />
+					</main>
+				)
+			) : (
+				<Navigate to={"/login"} />
+			)}
+		</div>
+	);
 }
