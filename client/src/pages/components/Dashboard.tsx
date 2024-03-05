2cae8ec7dcdc2861b05335b127cae2f85936ed75
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-03-05 15:09:14 +0100

    Get id from param rather than firebase

--- client/src/pages/components/Dashboard.tsx
+++ client/src/pages/components/Dashboard.tsx
@@ -5,6 +5,8 @@ import { Spin } from "antd";
 import StudentDashboard from "../../features/dashboard/components/StudentDashboard";
 import ExaminerDashboard from "../../features/dashboard/components/ExaminerDashboard";
 import ErrorBoundary from "../../components/error/ErrorBoundary.small";
+import CreateExamButton from "../../features/dashboard/components/CreateExamButton";
+import { useParams } from "react-router-dom";
 
 const FETCH_USER_ROLE = gql`
 	query fetchUserRole($id: ID!) {
@@ -15,15 +17,17 @@ const FETCH_USER_ROLE = gql`
 `;
 
 export default function Dashboard() {
+	const { id } = useParams();
 	const { currentUser } = useAuth();
 	const { loading, data, refetch } = useQuery(FETCH_USER_ROLE, {
-		variables: { id: currentUser?.uid },
+		variables: { id: id },
 	});
 
 	return (
 		<div id="dashboard">
 			<header>
 				<h1>Dashboard</h1>
+				{data && data.user.role === "EXAMINER" ? <CreateExamButton /> : null}
 				<small>Good to see you here</small>
 			</header>
 			{loading ? (
