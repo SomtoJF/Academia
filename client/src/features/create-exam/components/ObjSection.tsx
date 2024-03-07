07e32ebfc5eeb477789bd7815b610572b084146e
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-03-07 22:15:24 +0100

    Initial commit

---
+++ client/src/features/create-exam/components/ObjSection.tsx
@@ -0,0 +1,32 @@
+import { PlusOutlined } from "@ant-design/icons";
+import { ObjectiveQuestions } from "../../../types";
+import "./ObjSection.styles.sass";
+import { useState } from "react";
+import ExamModal from "./ExamModal";
+
+interface Props {
+	objQuestions: ObjectiveQuestions[];
+	setObjQuestions: (objQuestions: ObjectiveQuestions[]) => void;
+}
+
+export default function ObjSection({}: Props) {
+	const [isModalOpen, setIsModalOpen] = useState(false);
+	return (
+		<section id="obj-section">
+			<h2>Multiple Choice Section</h2>
+			<button
+				type="button"
+				id="new-obj-question"
+				onClick={() => setIsModalOpen(true)}
+			>
+				<PlusOutlined />
+				New question
+			</button>
+			<ExamModal
+				isModalOpen={isModalOpen}
+				setIsModalOpen={setIsModalOpen}
+				title="New Objective Question"
+			/>
+		</section>
+	);
+}
