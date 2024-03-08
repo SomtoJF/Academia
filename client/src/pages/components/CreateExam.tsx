256a016bdb7bd80a3f67388c34053d9a0f629eaf
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-03-08 01:12:19 +0100

    Save draft

--- client/src/pages/components/CreateExam.tsx
+++ client/src/pages/components/CreateExam.tsx
@@ -2,10 +2,12 @@ import { useParams } from "react-router-dom";
 import ExaminerInfo from "../../features/create-exam/components/ExaminerInfo";
 import "../styles/CreateExam.styles.sass";
 import { gql, useMutation, useQuery } from "@apollo/client";
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { LoadingOutlined } from "@ant-design/icons";
 import { Exam, ObjectiveQuestions } from "../../types";
 import ObjSection from "../../features/create-exam/components/ObjSection";
+import storageAvailable from "../../utils/storage-available";
+import { message } from "antd";
 const FETCH_USER_DATA = gql`
 	query fetchUserRole($id: ID!) {
 		user(id: $id) {
@@ -34,6 +36,7 @@ export default function CreateExam() {
 	>([]);
 	const [dueDate, setDueDate] = useState<Date>();
 	const [submitLoading, setSubmitLoading] = useState(false);
+	const [messageApi, contextHolder] = message.useMessage();
 	const [createExam] = useMutation(CREATE_EXAM_MUTATION);
 	const { id } = useParams();
 
@@ -46,6 +49,54 @@ export default function CreateExam() {
 			}
 		},
 	});
+
+	useEffect(() => {
+		if (localStorage.getItem("exam-name")) {
+			getDraft();
+			successMesage("Loaded successfully from draft");
+		}
+	}, []);
+
+	const errorMesage = (message: string) => {
+		messageApi.open({
+			type: "error",
+			content: message,
+		});
+	};
+	const successMesage = (message: string) => {
+		messageApi.open({
+			type: "success",
+			content: message,
+		});
+	};
+
+	const getDraft = () => {
+		if (localStorage.getItem("exam-name"))
+			setExamName(localStorage.getItem("exam-name")!);
+		if (localStorage.getItem("due-date"))
+			setDueDate(JSON.parse(localStorage.getItem("due-date")!));
+		if (localStorage.getItem("objective-questions"))
+			setObjectiveQuestions(
+				JSON.parse(localStorage.getItem("objective-questions")!)
+			);
+	};
+
+	const handleSaveDraft = () => {
+		try {
+			if (!storageAvailable("localStorage"))
+				throw new Error("Cannot save draft");
+			localStorage.setItem("exam-name", examName);
+			if (dueDate) localStorage.setItem("due-date", JSON.stringify(dueDate));
+			if (objectiveQuestions.length > 0)
+				localStorage.setItem(
+					"objective-questions",
+					JSON.stringify(objectiveQuestions)
+				);
+			successMesage("Saved successfully");
+		} catch (err: any) {
+			errorMesage(err.message);
+		}
+	};
 
 	const handleSubmit = (e: FormEvent) => {
 		e.preventDefault();
@@ -69,32 +120,48 @@ export default function CreateExam() {
 	if (error) throw new Error("Something went wrong while validating user role");
 
 	return (
-		<div id="create-exam">
-			{loading ? (
-				<div id="loading-container">
-					<LoadingOutlined />
-				</div>
-			) : data ? (
-				<ExaminerInfo
-					firstName={data.user.firstName}
-					lastName={data.user.lastName}
-					email={data.user.email}
-					profilePicture={data.user.profilePicture}
-					examName={examName}
-					setExamName={setExamName}
-					setDueDate={setDueDate}
-				/>
-			) : null}
-			<h1>Create a new exam</h1>
-			<form>
-				<ObjSection
-					objQuestions={objectiveQuestions}
-					setObjQuestions={setObjectiveQuestions}
-				/>
-				<button type="submit" disabled={submitLoading} onClick={handleSubmit}>
-					{submitLoading ? <LoadingOutlined /> : "Create Exam"}
-				</button>
-			</form>
-		</div>
+		<>
+			{contextHolder}
+			<div id="create-exam">
+				{loading ? (
+					<div id="loading-container">
+						<LoadingOutlined />
+					</div>
+				) : data ? (
+					<ExaminerInfo
+						firstName={data.user.firstName}
+						lastName={data.user.lastName}
+						email={data.user.email}
+						profilePicture={data.user.profilePicture}
+						examName={examName}
+						setExamName={setExamName}
+						setDueDate={setDueDate}
+					/>
+				) : null}
+				<h1>Create a new exam</h1>
+				<form>
+					<ObjSection
+						objQuestions={objectiveQuestions}
+						setObjQuestions={setObjectiveQuestions}
+					/>
+					<button
+						type="button"
+						className="new-exam-control"
+						onClick={handleSaveDraft}
+					>
+						Save Draft
+					</button>
+
+					<button
+						type="submit"
+						className="new-exam-control"
+						disabled={submitLoading}
+						onClick={handleSubmit}
+					>
+						{submitLoading ? <LoadingOutlined /> : "Create Exam"}
+					</button>
+				</form>
+			</div>
+		</>
 	);
 }
