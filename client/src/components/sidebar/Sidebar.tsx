8776b72a76ae65f213137c821b95840a08599d8f
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-03-17 15:48:46 +0100

    Throttle logout

--- client/src/components/sidebar/Sidebar.tsx
+++ client/src/components/sidebar/Sidebar.tsx
@@ -14,11 +14,20 @@ import { useAuth } from "../../contexts/AuthContext";
 import { NavLink, useParams } from "react-router-dom";
 import gsap from "gsap";
 import { useState } from "react";
+import { message } from "antd";
 
 export default function Sidebar() {
 	const { logout } = useAuth();
 	const { id } = useParams();
 	const [isSidebarExpanded, setSideBarExpanded] = useState(false);
+	const [messageApi, contextHolder] = message.useMessage();
+
+	const success = (message: string) => {
+		messageApi.open({
+			type: "success",
+			content: message,
+		});
+	};
 
 	const expandSideBar = () => {
 		gsap.to("#sidebar", {
@@ -46,58 +55,68 @@ export default function Sidebar() {
 		}, 500);
 	};
 
+	const handleLogout = async () => {
+		success("Successfully logged out");
+		setTimeout(() => {
+			logout();
+		}, 1000);
+	};
+
 	return (
-		<aside id="sidebar">
-			<ul>
-				<li onClick={isSidebarExpanded ? collapseSideBar : expandSideBar}>
-					<a href="" style={{ pointerEvents: "none" }}>
-						{isSidebarExpanded ? (
-							<VerticalRightOutlined />
-						) : (
-							<VerticalLeftOutlined />
-						)}{" "}
-						<span className="link-text">
-							{isSidebarExpanded ? "Collapse" : null}
-						</span>
-					</a>
-				</li>
-				<li onClick={collapseSideBar}>
-					<NavLink to={"/"}>
-						<HomeOutlined /> <span className="link-text">Home</span>
-					</NavLink>
-				</li>
-				<li onClick={collapseSideBar}>
-					<NavLink to={`/user/${id}/dashboard`}>
-						<AppstoreOutlined /> <span className="link-text">Dashboard</span>
-					</NavLink>
-				</li>
-				<li onClick={collapseSideBar}>
-					<NavLink to={"/hsjs"}>
-						<DotChartOutlined /> <span className="link-text">Results</span>
-					</NavLink>
-				</li>
-				<li onClick={collapseSideBar}>
-					<NavLink to={`/user/${id}/schedule`}>
-						<CalendarOutlined /> <span className="link-text">Schedule</span>
-					</NavLink>
-				</li>
-				<li onClick={collapseSideBar}>
-					<NavLink to={`/user/${id}/account`}>
-						<UserOutlined /> <span className="link-text">My Account</span>
-					</NavLink>
-				</li>
-				<li onClick={collapseSideBar}>
-					<NavLink to={`/user/${id}/faqs`}>
-						<QuestionOutlined />
-						<span className="link-text">FAQs</span>
-					</NavLink>
-				</li>
-			</ul>
-			<div id="auth-container">
-				<button id="logout" onClick={logout}>
-					<DisconnectOutlined /> <span className="link-text">Logout</span>
-				</button>
-			</div>
-		</aside>
+		<>
+			{contextHolder}
+			<aside id="sidebar">
+				<ul>
+					<li onClick={isSidebarExpanded ? collapseSideBar : expandSideBar}>
+						<a href="" style={{ pointerEvents: "none" }}>
+							{isSidebarExpanded ? (
+								<VerticalRightOutlined />
+							) : (
+								<VerticalLeftOutlined />
+							)}{" "}
+							<span className="link-text">
+								{isSidebarExpanded ? "Collapse" : null}
+							</span>
+						</a>
+					</li>
+					<li onClick={collapseSideBar}>
+						<NavLink to={"/"}>
+							<HomeOutlined /> <span className="link-text">Home</span>
+						</NavLink>
+					</li>
+					<li onClick={collapseSideBar}>
+						<NavLink to={`/user/${id}/dashboard`}>
+							<AppstoreOutlined /> <span className="link-text">Dashboard</span>
+						</NavLink>
+					</li>
+					<li onClick={collapseSideBar}>
+						<NavLink to={"/hsjs"}>
+							<DotChartOutlined /> <span className="link-text">Results</span>
+						</NavLink>
+					</li>
+					<li onClick={collapseSideBar}>
+						<NavLink to={`/user/${id}/schedule`}>
+							<CalendarOutlined /> <span className="link-text">Schedule</span>
+						</NavLink>
+					</li>
+					<li onClick={collapseSideBar}>
+						<NavLink to={`/user/${id}/account`}>
+							<UserOutlined /> <span className="link-text">My Account</span>
+						</NavLink>
+					</li>
+					<li onClick={collapseSideBar}>
+						<NavLink to={`/user/${id}/faqs`}>
+							<QuestionOutlined />
+							<span className="link-text">FAQs</span>
+						</NavLink>
+					</li>
+				</ul>
+				<div id="auth-container">
+					<button id="logout" onClick={handleLogout}>
+						<DisconnectOutlined /> <span className="link-text">Logout</span>
+					</button>
+				</div>
+			</aside>
+		</>
 	);
 }
