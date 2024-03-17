71260c9ce398de14f2cf8ba8a85cdf6374629272
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-03-17 18:00:49 +0100

    BugFix: overview breaks on examiner dashboard

--- client/src/features/dashboard/components/Overview.tsx
+++ client/src/features/dashboard/components/Overview.tsx
@@ -1,27 +1,32 @@
 import {
 	CheckCircleOutlined,
-	ClockCircleOutlined,
 	IssuesCloseOutlined,
 	PlusCircleOutlined,
+	WarningOutlined,
 } from "@ant-design/icons";
 import "./Overview.styles.sass";
 
 interface Props {
 	concludedExams: any[];
 	upcomingExams: any[];
+	submittedExams?: any[];
 }
 
-export default function Overview({ upcomingExams, concludedExams }: Props) {
+export default function Overview({
+	upcomingExams,
+	concludedExams,
+	submittedExams,
+}: Props) {
 	return (
 		<section id="dashboard-overview">
 			<h3>Overview</h3>
 			<div id="overview-container">
 				<article>
 					<div>
-						<CheckCircleOutlined />
+						<WarningOutlined />
 					</div>
 					<p>{concludedExams.length}</p>
-					<p>Exams concluded</p>
+					<p>Exams Ended</p>
 				</article>
 				<article>
 					<div>
@@ -37,13 +42,15 @@ export default function Overview({ upcomingExams, concludedExams }: Props) {
 					<p>{concludedExams.length + upcomingExams.length}</p>
 					<p>Exams Registered for</p>
 				</article>
-				<article>
-					<div>
-						<ClockCircleOutlined />
-					</div>
-					<p>{concludedExams.length}</p>
-					<p>Exams concluded</p>
-				</article>
+				{submittedExams && (
+					<article>
+						<div>
+							<CheckCircleOutlined />
+						</div>
+						<p>{submittedExams.length}</p>
+						<p>Exams submitted</p>
+					</article>
+				)}
 			</div>
 		</section>
 	);
